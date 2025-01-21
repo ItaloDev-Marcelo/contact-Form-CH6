9568ead9f66fe61c925b2741e3f1ea7e809421f9
Italo Marcelo <devfrontend2028study@gmail.com>
2025-01-21 10:50:36 -0300

    up

--- src/pages/use-simple-form/index.jsx
+++ src/pages/use-simple-form/index.jsx
@@ -9,15 +9,16 @@ export default function ContactForm() {
         reset()
     }
 
+    console.log(errors)
+
     return (
-        <div id="container">
-            <h1>Contact us</h1>
+        
             <form id="form--container" onSubmit={handleSubmit(onSubmit)}>
-           
+            <legend>Contact us</legend>
            <div className="grid-1">
            <div className="input--row1">
             <label htmlFor="firstName">First Name *</label>
-           <input style={{outlineColor: errors.firstName ? 'red' : 'green'}} {...register('firstName', {
+           <input style={{outlineColor: errors.firstName ? 'red' : ''}} {...register('firstName', {
             required: true
            })} name='firstName' id='firstName' type='text' />
            {errors.firstName ? <p>This field is required</p> : null}
@@ -28,7 +29,7 @@ export default function ContactForm() {
           <div className="grid-2">
           <div className="input--row2">
             <label htmlFor="lastName">Last Name *</label>
-           <input style={{outlineColor: errors.lastName ? 'red' : 'green'}} {...register('lastName', {
+           <input style={{outlineColor: errors.lastName ? 'red' : ''}} {...register('lastName', {
             required: true
            })} name='lastName' id='lastName' type='text' />
             {errors.lastName ? <p>This field is required</p> : null}
@@ -38,7 +39,7 @@ export default function ContactForm() {
           <div className="grid-3">
           <div className="input--row3">
             <label htmlFor="email">Email address *</label>
-           <input  style={{outlineColor: errors.email ? 'red' : 'green'}} {...register('email', {
+           <input  style={{outlineColor: errors.email ? 'red' : ''}} {...register('email', {
             required: true
            })} name={'email'} id={'email'} type={'email'} />
             {errors.email ? <p>please enter a valid email address</p> : null}
@@ -50,22 +51,22 @@ export default function ContactForm() {
             <label htmlFor="">Query Type *</label>
          
             <div id="Qr1">
-              <div className="space">
+              <div className="space" style={{outlineColor: errors.queryType ? 'red' : ''}}>
               <label htmlFor="queryType1">
              General Enquiry 
             </label>
-            <input {...register('queryType', {
+            <input  {...register('queryType', {
             required: true
            })} name='queryType' id='queryType1' type='radio' value='General Enquiry' />
               </div>
             </div>
 
             <div id="Qr2">
-             <div className="space">
+             <div className="space" style={{outlineColor: errors.queryType ? 'red' : ''}}>
              <label htmlFor="queryType2">
             Support Request
             </label>
-            <input {...register('queryType', {
+            <input  {...register('queryType', {
             required: true
            })} name='queryType' id='queryType2' type='radio' value='Support Request' />
              </div>
@@ -89,7 +90,7 @@ export default function ContactForm() {
 
           <div className="grid-6">
           <label className='check' htmlFor="confirmTerms">
-              <input style={{borderColor: errors.confirmCheckBox ? 'red' : 'green'}}  {...register('confirmCheckBox', {
+              <input style={{borderColor: errors.confirmCheckBox ? 'red' : ''}}  {...register('confirmCheckBox', {
             required: true
            })} name='confirmCheckBox' id='confirmTerms' type="checkbox" />
             I consent to being contacted by the team
@@ -101,6 +102,6 @@ export default function ContactForm() {
                      
            <button type="submit">Submit</button>
         </form>
-        </div>    
+        
     )
 }
